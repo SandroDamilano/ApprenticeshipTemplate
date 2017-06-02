689c4a9797fc22e32824592d4cc21e181a16b395
Sandro <damilano.sandro@gmail.com>
2017-06-02 17:28:20 -0300

    Agregados los CSSs para poder usar Materialize

--- holiday-calendar-ember/app/adapters/application.js
+++ holiday-calendar-ember/app/adapters/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  host: 'http://localhost:8080',
+  host: 'http://192.168.1.68:8080',
 
   pathForType: function(type) {
     return 'calendarios';
