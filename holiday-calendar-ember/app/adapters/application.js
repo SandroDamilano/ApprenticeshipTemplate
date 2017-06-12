b78edc16c3447bd0eb106dc8308c00a0d7459c74
MagdalenaMileto <maggiemileto@gmail.com>
2017-06-12 13:58:23 -0300

    asd2

--- holiday-calendar-ember/app/adapters/application.js
+++ holiday-calendar-ember/app/adapters/application.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 import UrlTemplates from "ember-data-url-templates";
 
 export default DS.RESTAdapter.extend(UrlTemplates, {
-  host: 'http://192.168.1.68:8080',
+  host: 'http://192.168.1.106:8080',
 });
