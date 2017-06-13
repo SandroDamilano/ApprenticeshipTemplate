2dea328970e77ea0964711285a0a8c1f743548e7
Sandro <damilano.sandro@gmail.com>
2017-06-13 17:00:48 -0300

    Cambiado el combo-box + botón por un dropdown

--- holiday-calendar-ember/app/components/drop-down.js
+++ holiday-calendar-ember/app/components/drop-down.js
@@ -1,20 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didRender(){
+    this._super(...arguments);
 
-  actions: {
-    didRender(){
-      $('.dropdown-button').dropdown({
-        inDuration: 300,
-        outDuration: 225,
-        constrainWidth: false, // Does not change width of dropdown to that of the activator
-        hover: true, // Activate on hover
-        gutter: 0, // Spacing from edge
-        belowOrigin: false, // Displays dropdown below the button
-        alignment: 'left', // Displays dropdown with edge aligned to the left of button
-        stopPropagation: false // Stops event propagation probando
-      });
-    }
+    $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: false // Stops event propagation
+    });
   }
 
 });
